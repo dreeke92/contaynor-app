e268b33eb80af388bb4fc12b4b7415604b923a34
Andreas Van Assche <andreasava@hotmail.com>
2020-01-16 08:08:57 +0100

    changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,21 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new()
-    @addresses = current_user.organization.addresses
   end
 
   def create
+    raise
     @order = Order.new(order_params)
     @order.user = current_user
     @order.organization = current_user.organization
     @order.status = "Pending"
     @order.save
-    raise
     redirect_to root_path
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:pickup_date, :pickup_time, :pickup_address, :delivery_address, :comment)
+    params.require(:order).permit(:pickup_address)
   end
 end

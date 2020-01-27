d7d9e31d848124ed84bd4d22048ca27c32312b1d
Chuks Opia <chuks24ng@yahoo.co.uk>
2020-01-27 19:08:46 +0100

    - Change order table status column to int - Make status column an active record enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,11 @@
 class Order < ApplicationRecord
+  enum status: { requesting: 0, pending: 1, accepted: 2, performed: 3 }
+
   belongs_to :user
   belongs_to :pickup_address, class_name: "Address"
   belongs_to :delivery_address, class_name: "Address"
   belongs_to :organization
   has_many :transport_loads
+
+  validates :pickup_date, :pickup_time, presence: true
 end
